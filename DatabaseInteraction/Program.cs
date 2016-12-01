d4f34d0515c52d28788edc8aace86764f801bcd7
Jacob Morehouse <jacob.morehouse@gmail.com>
2016-11-30 20:44:48 -0800

    Rearranged into a more OO friendly construction.

--- DatabaseInteraction/Program.cs
+++ DatabaseInteraction/Program.cs
@@ -1,6 +1,7 @@
 ﻿/* References
  * 
  * SQL queries https://stackoverflow.com/questions/18754688/c-sharp-how-to-implement-method-that-return-list-of-sql-result
+ * SQL queries on MSDN https://msdn.microsoft.com/en-us/library/fksx3b4f.aspx
  * 
  */
 
@@ -23,6 +24,100 @@ namespace DatabaseInteraction
             public string password { get; set; }
             public string email { get; set; }
         }
+
+        public class dbAction
+        {
+            private static string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=localtest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            public static List<Person> listusers()
+            {
+
+
+                SqlConnection sqlConnection1 = new SqlConnection(ConnectionString);
+                SqlCommand cmd = new SqlCommand();
+                SqlDataReader reader;
+
+                cmd.CommandText = "SELECT id, username, password, email FROM Users";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = sqlConnection1;
+
+                sqlConnection1.Open();
+
+                reader = cmd.ExecuteReader();
+                // Data is accessible through the DataReader object.
+
+                //first instantiate a generic list to store the Person objects
+                var listOfPerson = new List<Person>();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        //build our new person object for each row
+                        var person = new Person();
+                        person.username = reader["username"].ToString();
+                        person.password = reader["password"].ToString();
+                        person.id = Convert.ToInt32(reader["id"]);
+                        person.email = reader["email"].ToString();
+
+                        //add the person to the list
+                        listOfPerson.Add(person);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                reader.Close();
+
+                sqlConnection1.Close();
+
+                Console.WriteLine("\nList of users:");
+                foreach (var p in listOfPerson)
+                {
+                    Console.WriteLine(p.id + ", " + p.username + ", " + p.password + ", " + p.email);
+                }
+
+                return listOfPerson;
+
+            }
+            public static void exit()
+            {
+                Environment.Exit(0);
+            }
+            public static void adduser()
+            {
+                //first get the user input and build out the Person object
+                Person newPerson = new Person();
+                Console.Write("ID: ");
+                int userInputID; //this is all just to parse out the int.
+                var temp = Int32.TryParse(Console.ReadLine(), out userInputID);
+                newPerson.id = userInputID;
+                //Int32.TryParse(useraction, out numericResult)
+
+                Console.Write("Username: ");
+                newPerson.username = Console.ReadLine();
+                Console.Write("Password: ");
+                newPerson.password = Console.ReadLine();
+                Console.Write("Email: ");
+                newPerson.email = Console.ReadLine();
+
+
+                SqlConnection sqlConnection1 = new SqlConnection(ConnectionString);
+                SqlCommand cmd = new SqlCommand();
+
+                cmd.CommandText = String.Format("INSERT INTO Users(id, username, password, email) values ({0},'{1}','{2}','{3}')", newPerson.id, newPerson.username, newPerson.email, newPerson.password);
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = sqlConnection1;
+
+                sqlConnection1.Open();
+                cmd.ExecuteNonQuery();
+                sqlConnection1.Close();
+
+            }
+
+        }
+        
 
 
         static void Main(string[] args)
@@ -62,71 +157,45 @@ namespace DatabaseInteraction
                 }
 
                 //see if we chose to exit
-                if (useraction == 'x'.ToString() || useraction == 'q'.ToString()) {
-                    break;
-                }
+                //if (useraction == 'x'.ToString() || useraction == 'q'.ToString()) {
+                //    break;
+                //}
                 
                 //see if we chose to clear screen
                 if (useraction == 'c'.ToString())
                 {
-                    Console.Clear();
+                    
                 }
 
 
-                //List users
-                if (useraction == 'l'.ToString())
+                switch (useraction)
                 {
-                    SqlConnection sqlConnection1 = new SqlConnection(myConnectionString);
-                    SqlCommand cmd = new SqlCommand();
-                    SqlDataReader reader;
+                    case "l":
+                        dbAction.listusers();
+                        break;
+                    case "q":
+                        dbAction.exit();
+                        break;
+                    case "x":
+                        dbAction.exit();
+                        break;
+                    case "c":
+                        Console.Clear();
+                        break;
+                    case "a":
+                        dbAction.adduser();
+                        break;
+                        
 
-                    cmd.CommandText = "SELECT id, username, password, email FROM Users";
-                    cmd.CommandType = CommandType.Text;
-                    cmd.Connection = sqlConnection1;
-
-                    sqlConnection1.Open();
-
-                    reader = cmd.ExecuteReader();
-                    // Data is accessible through the DataReader object.
-
-                    //first instantiate a generic list to store the Person objects
-                    var listOfPerson = new List<Person>();
-
-                    if (reader.HasRows)
-                    {
-                        while (reader.Read())
-                        {
-                            //build our new person object for each row
-                            var person = new Person();
-                            person.username = reader["username"].ToString();
-                            person.password = reader["password"].ToString();
-                            person.id = Convert.ToInt32(reader["id"]);
-                            person.email = reader["email"].ToString();
-
-                            //add the person to the list
-                            listOfPerson.Add(person);
-                        }
-                    }
-                    else 
-                    {
-                        Console.WriteLine("No rows found.");
-                    }
-                    reader.Close();
-
-                    sqlConnection1.Close();
-
-                    Console.WriteLine("\nList of users:");
-                    foreach (var p in listOfPerson)
-                    {
-                        Console.WriteLine(p.id + ", " + p.username + ", " + p.password + ", " + p.email);
-                    }
-
+                    default:
+                        break;
                 }
+
+
 
                 Console.WriteLine("\n");
             } while (true);
             
-            //commit test4
         }
     }
 }
