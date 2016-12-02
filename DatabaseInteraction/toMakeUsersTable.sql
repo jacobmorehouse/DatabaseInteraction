c8f634e3d42b7861dab21edf5a8bb81744b6bf7b
Jacob Morehouse <jacob.morehouse@gmail.com>
2016-12-01 18:49:59 -0800

    added SQL file to build the Users table

---
+++ DatabaseInteraction/toMakeUsersTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT          NOT NULL,
+    [username] VARCHAR (50) NOT NULL,
+    [password] VARCHAR (50) NOT NULL,
+    [email]    VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
